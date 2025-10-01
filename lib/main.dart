a4507f5f1f1f35799602725bea370e01ffaaec02
BHIMANTARA435 <bhimantara.bhima21@gmail.com>
2025-10-01 09:23:28 +0700

    P1-L2-JS5-Bhimantara

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout: Bhimantara 2341760025',
+      title: 'Flutter layout: Nama dan NIM Anda',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
