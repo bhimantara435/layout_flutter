bc982c0209b9c50ba5dd4cae4a4a55757325ff7d
BHIMANTARA435 <bhimantara.bhima21@gmail.com>
2025-10-01 09:37:06 +0700

    P1-L4-JS5-Bhimantara (Soal 1.2.3)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,14 +7,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ===== Title Section =====
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32), // padding 32
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1 */
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // mulai dari kiri
+              children: [
+                /* soal 2 */
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8), // padding bawah 8
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey, // warna abu-abu
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3 */
+          const Icon(
+            Icons.star,
+            color: Colors.red, // warna merah
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    // ===== Return MaterialApp =====
     return MaterialApp(
-      title: 'Flutter layout: Nama dan NIM Anda',
+      title: 'Flutter layout: Bhimantara 2341760025',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: [
+            titleSection, // tampilkan titleSection
+          ],
         ),
       ),
     );
